9725e725b2530d98a8eb5de6434bbc8ea5a69a1b
rbian <rbian@redhat.com>
2015-02-06 18:07:55 +0800

    Modify ci.py to force update config files by bootstrap function.

--- ci.py
+++ ci.py
@@ -991,7 +991,8 @@ class LibvirtCI():
                             selinux=True,
                             restore_image=False,
                             verbose=True,
-                            update_providers=False)
+                            update_providers=False,
+                            force_update=True)
         os.chdir(data_dir.get_root_dir())
 
     def prepare_env(self):
@@ -1009,10 +1010,6 @@ class LibvirtCI():
 
         utils_libvirtd.Libvirtd().restart()
         service.Factory.create_service("nfs").restart()
-
-        print 'Running bootstrap'
-        sys.stdout.flush()
-        self.bootstrap()
 
         restore_image = True
         if self.args.img_url:
@@ -1046,6 +1043,10 @@ class LibvirtCI():
                 "shared/cfg/base.cfg",
                 r'^\s*vms = .*\n',
                 r'vms = %s\n' % vms_string)
+
+        print 'Running bootstrap'
+        sys.stdout.flush()
+        self.bootstrap()
 
         if self.args.retain_vm:
             return
